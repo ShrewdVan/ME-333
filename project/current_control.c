7111b7cf7f06a76cfa8ca4274b02ea4c78a3a8b9
ShrewdVan <168167150+ShrewdVan@users.noreply.github.com>
2025-03-16 02:38:29 -0500

    Update current_control.c

--- project/current_control.c
+++ project/current_control.c
@@ -23,50 +23,67 @@ static volatile int new_duty = 0;
 
 
 
-
+// ISR function for the 20kHZ current control
 void __ISR(_TIMER_3_VECTOR, IPL4SOFT) Timer3ISR(void){
     IFS0bits.T3IF = 0;
     switch (get_mode()){
-
+        // When mode is IDLE, set PWM = 0 to make sure motor will not rotate 
         case IDLE:
         {
             OC1RS = 0;
             break;
         }
-
+        // Mapping the percentage given by user to the PR2 and adjust the OC1RS, controlling the rotating speed
+        // Set another DIO output to control the direction of the motor by setting the it to be 1 for counter-clockwise and 0 for clockwise
         case PWM:
         {
             if (PWM_requested > 0){
+                // Calculate the OC1RS corresponding to the persentage
                 OC1RS = (int)((float)(PWM_requested * PR2)/100.0);
+                // Set the direction
                 LATBbits.LATB12 = 1;
             } else{
+                // Calculate the OC1RS corresponding to the persentage
                 OC1RS = (int)((float)((-1) * PWM_requested * PR2)/100.0);
+                // Set the direction
                 LATBbits.LATB12 = 0;}
             break;
         }
 
+        // When the mode is ITEST, Itest_counter is responsible for turning the mode back to IDLE when requried number of sample is collected
         case ITEST:
         {
+            // When the number hits 100, set the mode back to IDLE 
             if (Itest_counter == 100){
+                // clear the counter for the next ITEST 
                 Itest_counter = 0;
+                // clear the intergal error for the next ITEST or TRACK
                 E_accu_current = 0;
 
                 set_mode(IDLE);
                 break;
             }
 
+            // Real current
             Real_Itest = INA219_read_current();
+            // Store the real current
             Current_Actual_ITEST[Itest_counter] = Real_Itest;
+            // Calculate the error
             e_current = Current_Reference_ITEST[Itest_counter] - Real_Itest;
+            // Accumulate the integral error
             E_accu_current += e_current;
-            
+
+            // Calculate the required current for the next step
             new_current = Current_Kp * e_current + Current_Ki * E_accu_current;
+
+            // Same procedure as PWM 
             if (new_current > 0){
                 LATBbits.LATB12 = 1;
             } else if (new_current < 0) {
                 LATBbits.LATB12 = 0;
                 new_current = abs(new_current);
             }
+            // Cap Algrothim for current to avoid a invalid input
             if (new_current > 100.0){
                 new_current = 100.0;
             }
@@ -76,6 +93,8 @@ void __ISR(_TIMER_3_VECTOR, IPL4SOFT) Timer3ISR(void){
             break;
         }
 
+        // When the mode is HOLD, use the shared variable "Required_current_HOLD" calculated by position control to get the latest reuqired current
+        // Code is identical to the ITEST expect the required current is now given by position control instead of ITEST waveform
         case HOLD:
         {
             Real_HOLD = INA219_read_current();
@@ -98,6 +117,8 @@ void __ISR(_TIMER_3_VECTOR, IPL4SOFT) Timer3ISR(void){
 
         }
 
+        // When the mode is TRACK, basically it's like multiple HOLD cases stack up. For current control loop there's no difference, therefore
+        //   the code would also unchanged.
         case TRACK:
         {
             Real_HOLD = INA219_read_current();
@@ -125,9 +146,11 @@ void __ISR(_TIMER_3_VECTOR, IPL4SOFT) Timer3ISR(void){
 
 }
 
+// Function used to generate the desired current for the ITEST
 void ITEST_Current_Wave(){
+    
     float Reference_Itest = 200.0;
-
+    // When the Reference_Generator_Counter hits 25, 50, and 75, convert the sign
     for (int Reference_Generator_Counter = 0; Reference_Generator_Counter <= 99; Reference_Generator_Counter++){
         if (Reference_Generator_Counter <= 24){
             Current_Reference_ITEST[Reference_Generator_Counter] = Reference_Itest;
@@ -145,6 +168,7 @@ void ITEST_Current_Wave(){
 }
 
 
+// Startup function to initialize the current control loop
 void current_control_Startup(){
     RPA0Rbits.RPA0R = 0b0101;
     // Timer 2 for the PWM control with a frequency of 20KHz
@@ -171,6 +195,7 @@ void current_control_Startup(){
     IFS0bits.T3IF = 0;
     IEC0bits.T3IE = 1;
 
+    // Pin RB12 is set to be a DIO output to control the direction of the motor
     TRISBbits.TRISB12 = 0;
     LATBbits.LATB12 = 0;
     
